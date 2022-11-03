87a5a317bd372d4147209dd333c88949b69099e6
Makhdoom <makhdoomansari45@gmail.com>
2022-11-04 00:59:24 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,31 @@
+function updateMap() {
+    console.log("Updating map with realtime data");
+    fetch("/data.json")
+        .then(response => response.json())
+        .then(rsp => {
+            // console.log(rsp.data)
+            rsp.data.forEach(element => {
+                latitude = element.latitude;
+                longitude = element.longitude;
+
+                cases = element.infected;
+                if (cases>200){
+                    color ="rgb(255, 26, 26)"
+                }
+                else{
+                    color = `rgb(${cases}, 83, 26)`;
+                }
+
+
+                //Mark on the Map
+                const marker = new mapboxgl.Marker({
+                    draggable: false,
+                    color: color,
+                })
+                    .setLngLat([longitude, latitude])
+                    .addTo(map);
+            });
+        })
+}
+let interval = 2000;
+setInterval(updateMap, interval);
